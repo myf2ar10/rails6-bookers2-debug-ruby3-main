aad39ac31edac4446e74c3ddbdc1ed27adf00b2d
EC2 Default User <ec2-user@ip-172-31-6-109.ap-northeast-1.compute.internal>
2023-10-04 06:23:39 +0000

    202310041523

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,49 @@ class User < ApplicationRecord
 
   # belongs_to :books
   has_many :books
+  has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_one_attached :profile_image
-  
 
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+
+  # validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :name, length: { minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+
+
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
 devise_for :users  #
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
-  resource :favorites, only: [:create, :destroy]
-end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create]
+  end
 
+  resources :users, only: [:index,:show,:edit,:update] do
+    resources :relationships, only: [:create,:destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
