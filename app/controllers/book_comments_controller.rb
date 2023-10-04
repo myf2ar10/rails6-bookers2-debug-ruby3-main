01b7016bff6fa1cb51e5d8774cb8a0222d669a48
EC2 Default User <ec2-user@ip-172-31-6-109.ap-northeast-1.compute.internal>
2023-10-04 07:08:21 +0000

    202310041608

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
-    post_image = PostImage.find(params[:post_image_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_image_id = post_image.id
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
     comment.save
-    redirect_to post_image_path(post_image)
+    redirect_to request.referer
+    # redirect_to book_path(book)
   end
 
   def destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    redirect_to request.referer
   end
-  
-  private  
-  
-  def post_comment_params
-    params.require(:post_comment).permit(:comment)
+
+  private
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
   end
-  
 end
